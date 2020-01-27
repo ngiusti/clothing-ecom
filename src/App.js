4be3afedf536d9c3269cbe3e029fcf402dec802e
Nicholas Giusti <nicholas.giusti89@gmail.com>
2020-01-26 21:54:48 -0800

    begun building sign-in form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component'
+import signInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/sign-in' component={signInAndSignUpPage} />
       </Switch>
     </div>
   );
